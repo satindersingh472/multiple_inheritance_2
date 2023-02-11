3751b4d6bc197e5a62e1fe306bae76ec26947a89
satinder <satindersingh472@gmail.com>
2023-02-11 10:09:51 -0700

    finished

--- app.py
+++ app.py
@@ -3,13 +3,39 @@
 class Employee:
     # new_id will 
     new_id = 1
+    # it will set the id of an employee to employee.new_id
+    # and auto increment the id everytime the code runs
     def __init__(self):
         self.id = Employee.new_id
         Employee.new_id += 1
 
+    # method say_id will define the instance 
     def say_id(self):
         print("My id is {}".format(self.id))
 
-e1 = Employee()
+
+class Admin(Employee):
+    def __init__(self):
+        super().__init__('Admin')
 
 
+class User:
+    def __init__(self,username,role="customer"):
+        self.username = username
+        self.role = role
+
+    def say_user_info(self):
+        print("My username is {}".format(self.username))
+        print("My role is {}".format(self.role))
+
+
+class Manager(Employee,User):
+    def __init__(self):
+        super().__init__()
+        User.__init__(self,self.id,'Manager')
+    def say_id(self):
+        User.say_user_info(self)
+
+
+m1 = Manager()
+m1.say_id()
--- readme.md
+++ readme.md
@@ -10,8 +10,17 @@ def a method called say_id(self) which can say something about that particular i
 
 
 
-create a class named **Admin**
+create a class named **Admin**  <br>
 def __init__ method <br>
 Admin should be inherited from class **Employee** but there should be some difference that admin can define <br>
 
 
+create a class named **User** <br>
+def __init__ method with self, username and role as parameters <br>
+def say_user_info() method which will describe the user <br>
+by default user is the customer so role will be customer as default value <br>
+
+create a class called **Manager** <br>
+this class should be inherited from **Employee** and **user** 
+which means a manager can be a user of the application and employee of an organization<br>
+now describe the id of the manager and role of the manager as a user <br>
